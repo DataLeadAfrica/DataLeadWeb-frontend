060a365b0b28da62f235695062aa99ee70a192fe
Data-Lead Africa <107042991+DATALEAD-AFRICA@users.noreply.github.com>
2026-07-11 12:04:37 +0100

    Update component.tsx

--- src/components/Partners/component.tsx
+++ src/components/Partners/component.tsx
@@ -8,6 +8,9 @@ import image_5 from "./assets/5.png";
 import image_6 from "./assets/6.png";
 import image_7 from "./assets/7.png";
 import image_8 from "./assets/8.png";
+import image_9 from "./assets/9.png";
+import image_10 from "./assets/10.png";
+import image_11 from "./assets/11.png";
 
 const images = [
   image_1,
@@ -18,17 +21,20 @@ const images = [
   image_6,
   image_7,
   image_8,
+  image_9,
+  image_10,
+  image_11,
 ];
 
 export default function Partners() {
   return (
     <div className="partners">
       <h2>Partners in change</h2>
-      <div>
-        {images.map((v) => {
+      <div className="partners__grid">
+        {images.map((v, i) => {
           return (
-            <div>
-              <img className="partners__partner" src={v} />{" "}
+            <div className="partners__cell" key={i}>
+              <img className="partners__partner" src={v} alt="" />
             </div>
           );
         })}
